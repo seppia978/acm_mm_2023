fcec269167ad67608efaf8eee3c5e0ca59be86df
sarasarto <sarasarto@gmail.com>
2023-04-20 11:18:04 +0200

    fixed wandb log and add number classes automatico

--- zero_shot_normal.py
+++ zero_shot_normal.py
@@ -198,7 +198,15 @@ def main(args):
 
     ret_acc, unl_acc = torch.zeros(1), torch.zeros(1)
 
-    all_classes = range(1)
+    if 'cifar10' in dataset.lower():
+        c_number = 10
+    elif 'cifar20' in dataset.lower():
+        c_number = 20
+    else:
+        print("Dataset not supported")
+        return -1
+    
+    all_classes = range(c_number)
 
     for class_to_delete in all_classes:
 
@@ -723,8 +731,8 @@ def main(args):
         # model.requires_grad_(requires_grad=False)
         # model = convert_conv2d_to_alpha(model, m=hyperparams['alpha_init'])
         
-        root='//mnt/beegfs/work/dnai_explainability/unlearning/icml2023/alpha_matrices/'
-        # root='//mnt/beegfs/work/dnai_explainability/ssarto/alpha_matrices/'
+        # root='//mnt/beegfs/work/dnai_explainability/unlearning/icml2023/alpha_matrices/'
+        root='//mnt/beegfs/work/dnai_explainability/ssarto/alpha_matrices/'
 
         if not os.path.isdir(os.path.join(root,wdb_proj)):
             os.mkdir(os.path.join(root,wdb_proj))
@@ -1176,7 +1184,7 @@ def main(args):
                             run.log({'best_val_acc': best_acc})
                             run.log({'current_val_acc': current_acc})
                             run.log({'best_acc_ret': mean_acc_keep})
-                            run.log({'best_acc_ret': mean_acc_forget})
+                            run.log({'best_acc_unl': mean_acc_forget})
 
                         if should_stop:
                             print(f'mean_unl: {mean_acc_forget}, current: {currents}, best: {best_acc}, patience: {patience}')
@@ -1213,7 +1221,7 @@ def main(args):
         PATH = f"{run_root}/final.pt"
         torch.save(model.state_dict(), PATH)
         print(f'Saved at {PATH}')
-        print(model.weights)
+        # print(model.weights)
         wandb.log({
             "mean_ret_acc": ret_acc/len(tuple(all_classes)),
             "mean_unl_acc": unl_acc/len(tuple(all_classes))
