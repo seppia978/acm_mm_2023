5e368af11b8e32cbf0718a4364c42a4bd4f243e0
seppia978 <s.poppi94@gmail.com>
2023-04-22 17:07:19 +0200

    added lora linear to classifier

--- custom_archs/non_imagenet_models/resnet_lora.py
+++ custom_archs/non_imagenet_models/resnet_lora.py
@@ -79,7 +79,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2, lora_r=lora_r)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2, lora_r=lora_r)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2, lora_r=lora_r)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = lora.Linear(512*block.expansion, num_classes, r=lora_r)
 
     def _make_layer(self, block, planes, num_blocks, stride, lora_r):
         strides = [stride] + [1]*(num_blocks-1)
